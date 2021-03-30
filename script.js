9542c749b571e30a89acf5d767c6b4e105004f6c
Aarush Hadimani <ahadimani2026@k12.andoverma.us>
2021-03-30 14:07:30 +0000

    normal changes

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let img='';
 let explanation=''
 
-axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+axios.get('https://api.nasa.gov/planetary/apod?api_key=RKgdwJgpccxKXckFqQNjb2wEbPTZcaF8b7kC8DbF')
   .then(response => {
     img = response.data.url;
     explanation = response.data.explanation;
     document.querySelector('img').src = img;
-    document.querySelector('p').innerText = explanation;
+    document.querySelector('#content').innerText = explanation;
   })
   .catch(error => console.log(error))
